7fe3c11e192252778a00dd1cad48f21e86fbafbe
Artur2 <arturnurullin@outlook.com>
2026-09-24 02:10:39 +0500

    Create static regex once lazily

--- src/common/utils.rs
+++ src/common/utils.rs
@@ -1,11 +1,12 @@
 use crate::data::error::DataPipeError::ClientIdentificationError;
 use crate::data::error::DataPipeResult;
+use std::sync::LazyLock;
 
 pub fn get_parameter_from_query(query: &str, name_of_parameter: &str) -> DataPipeResult<String> {
     use regex::Regex;
-    let regex = Regex::new(r"[?&]([^=#]+)=([^&#]*)").unwrap();
+    static REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"[?&]([^=#]+)=([^&#]*)").unwrap());
 
-    for caps in regex.captures_iter(query) {
+    for caps in REGEX.captures_iter(query) {
         if let Some(cap) = caps.get(1) {
             if cap.as_str() == name_of_parameter {
                 if let Some(id) = caps.get(2) {
