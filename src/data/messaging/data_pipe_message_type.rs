b9c6963349d8edacfd29ece7b30978df6527183e
Artur2 <arturnurullin@outlook.com>
2026-09-26 06:40:05 +0500

    Removed recursive call

--- src/data/messaging/data_pipe_message_type.rs
+++ src/data/messaging/data_pipe_message_type.rs
@@ -12,13 +12,13 @@ impl std::fmt::Display for DataPipeMessageType {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             DataPipeMessageType::Default => {
-                write!(f, "{0}", DataPipeMessageType::Default.to_string())
+                write!(f, "{0}", "Default")
             }
             DataPipeMessageType::Subscribe => {
-                write!(f, "{0}", DataPipeMessageType::Subscribe.to_string())
+                write!(f, "{0}", "Subscribe")
             }
             DataPipeMessageType::Unsubscribe => {
-                write!(f, "{0}", DataPipeMessageType::Unsubscribe.to_string())
+                write!(f, "{0}", "Unsubscribe")
             }
         }
     }
